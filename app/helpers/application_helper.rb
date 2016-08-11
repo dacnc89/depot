f3a02fdb35f3efb23ea7dc9068e4b8f2fec2ef1c
nguyendac8 <congdacit@gmail.com>
2016-08-11 10:42:20 +0700

    shopping cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_div_if(condition, atributes = {}, &block)
+    if condition
+      atributes["style"] = "display: none"
+    end
+    content_tag("div", atributes, &block)
+  end
 end
